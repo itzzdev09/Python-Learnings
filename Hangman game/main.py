957bed4f8865d024ef9688e656d9ec5fd765105f
Devraj Pal <devved90@gmail.com>
2024-01-03 22:08:43 +0530

    added loop and conditions

--- Hangman game/main.py
+++ Hangman game/main.py
@@ -2,3 +2,10 @@ import random_word_generator
 
 def change_current_word_state(selected_word,current_word_state,character):
     modified_word_state = ""
+    for i in range(len(selected_word)):
+        if current_word_state[i] == "_" and selected_word[i] == character:
+            modified_word_state += character
+        else:
+            modified_word_state += current_word_state[i]    
+
+    return modified_word_state    
